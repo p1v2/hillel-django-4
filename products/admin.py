2f848cda4dae1f5cc89135a2d8a4892feb8863e3
plaha303 <111688671+plaha303@users.noreply.github.com>
2024-08-14 21:02:44 +0300

    forgot to register models

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from products.models import Product, Category, Tag
+from products.models import Product, Category, Tag, Store, Inventory
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Tag)
+admin.site.register(Store)
+admin.site.register(Inventory)
