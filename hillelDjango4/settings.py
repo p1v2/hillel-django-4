2a87758117ccaf65d266ef166f42104a14851890
Vitalii Pavliuk <vitalii.pavliuk@preply.com>
2024-09-12 21:01:43 +0300

    secret key from environment

--- hillelDjango4/settings.py
+++ hillelDjango4/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$&1i*_nu88=2(m=oz#v=el)!mik4vi!wl6sy2tq$#ov9pi*-bl'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
